f66dae9e28afc43d4f5dd1ad32910a8fce4880b4
ROMEX <ROMEX@ROMEX-HP>
2015-06-22 12:35:36 +0200

    test_4

---
+++ src/lottery_project/IO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lottery_project;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ROMEX
+ */
+public class IO {
+    private Scanner sc;
+    public IO() {
+        sc = new Scanner(System.in);
+    }
+    public void printMenu(){
+        System.out.println("=== Lottery Menu ===\n1 Enter ticket\n2 Lottery drawing\n3 Show result\n Quit\n Please enter (0-3):");
+    }
+ 
+}
+    
